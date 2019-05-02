a42dffbf34b3c1297f98413e6747773715ceb84e
tyrants666 <akashyadav.iiita@gmail.com>
2019-05-02 15:19:54 +0545

    Hamburger Menu Animated

--- wp-content/themes/happyland/header.php
+++ wp-content/themes/happyland/header.php
@@ -64,10 +64,92 @@
 					</div>
 			</a>
 
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
+		  <div class="navbar-toggler" style="border-color: transparent;" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<svg viewBox="0 0 800 600">
+			    <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
+			    <path d="M300,320 L540,320" id="middle"></path>
+			    <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+			  </svg>
+		  </div>
 
+		<style media="screen">
+		.navbar-toggler {
+		background: rgba(0, 123, 255, 0.01);
+		margin: 0;
+		padding: 0;
+		text-align: center;
+		}
+		.navbar-toggler svg {
+		width: 94px;
+		height: 57px;
+		margin-right: -19px;
+		margin-left: -24px;
+		cursor: pointer;
+		-webkit-transform: translate3d(0, 0, 0);
+		-moz-transform: translate3d(0, 0, 0);
+		-o-transform: translate3d(0, 0, 0);
+		-ms-transform: translate3d(0, 0, 0);
+		transform: translate3d(0, 0, 0);
+		}
+		path {
+		fill: none;
+		-webkit-transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25);
+		-moz-transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25);
+		-o-transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25);
+		-ms-transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25);
+		transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25);
+		stroke-width: 30px;
+		stroke-linecap: round;
+		stroke: #808080;
+		stroke-dashoffset: 0px;
+		}
+		path#top,
+		path#bottom {
+		stroke-dasharray: 240px 950px;
+		}
+		path#middle {
+		stroke-dasharray: 240px 240px;
+		}
+		.cross path#top,
+		.cross path#bottom {
+		stroke-dashoffset: -650px;
+		stroke-dashoffset: -650px;
+		}
+		.cross path#middle {
+		stroke-dashoffset: -115px;
+		stroke-dasharray: 1px 220px;
+		}
+
+		</style>
+
+		<script type="text/javascript">
+
+		(function () {
+		  var i, resize;
+
+		  // i = setInterval(function () {
+		  //   return $(".navbar-toggler").toggleClass("cross");
+		  // }, 1500);
+
+		  $(".navbar-toggler").click(function () {
+		    clearInterval(i);
+		    return $(".navbar-toggler").toggleClass("cross");
+		  });
+
+		  resize = function () {
+		    return $("body").css({
+		      "margin-top": ~~((window.innerHeight - 150) / 2) + "px" });
+
+		  };
+
+		  $(window).resize(resize);
+
+		  resize();
+
+		}).call(this);
+
+		</script>
+		
 		  <div class="collapse navbar-collapse top-menu" id="navbarNavDropdown">
 				<?php wp_nav_menu( array('theme_location' => 'primary',
 				'depth'             => 2,
