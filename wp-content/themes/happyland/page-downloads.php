6370e155e9e38849aa8af939933a255209e0f507
tyrants666 <akashyadav.iiita@gmail.com>
2019-05-06 17:28:01 +0545

    Download page button height/padding fixed

--- wp-content/themes/happyland/page-downloads.php
+++ wp-content/themes/happyland/page-downloads.php
@@ -4,7 +4,7 @@
 
 <article class="containerr mt-125 mb-5">
 
-  <center><h2 class="h22 mb-5">Downloads</h2></center>
+  <center><h2 class="h22 mb-4">Downloads</h2></center>
 
 <div class="row mx-0">
 
@@ -15,7 +15,7 @@
       if( $file ): ?>
       <div class=" col-lg-3 col-sm-12 bg-img-prop-download  mx-auto">
         <a target="#" class="" href="<?php echo $file['url']; ?>">
-              <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/images/down1.jpg" class="" alt="Logo"/>
+              <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/images/down1.svg" class="" alt="Logo"/>
                   <p>Admisson Form</p>
         </a>
       </div>
