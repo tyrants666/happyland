c41a9eec613e829c2dd902a23dfa478fe5b96e08
tyrants666 <akashyadav.iiita@gmail.com>
2019-05-16 11:06:45 +0545

    Downloads page modified

--- wp-content/themes/happyland/page-downloads.php
+++ wp-content/themes/happyland/page-downloads.php
@@ -2,109 +2,120 @@
 
 <?php get_header(); ?>
 
-<article class="containerr mt-125 mb-5">
+<article class="containerr mt-125 mb-3 mb-lg-5">
 
   <center><h2 class="h22 mb-4 mb-lg-5">Downloads</h2></center>
 
 <div class="row mx-0">
 
 
-      <!-- Download Button 1 -->
-      <?php $file = get_field('admission_form'); ?>
-      <?php
-      if( $file ): ?>
-      <div class=" col-lg-3 col-11 bg-img-prop-download  mx-auto">
-        <a target="#" class="" href="<?php echo $file['url']; ?>">
-              <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/images/down1.svg" class="" alt="Logo"/>
-                  <p>Admisson Form</p>
-        </a>
-      </div>
-      <?php endif; ?>
-      <!-- Download Button 1 -->
-      <?php $file = get_field('calendar'); ?>
-      <?php
-      if( $file ): ?>
-      <div class=" col-lg-3 col-11 bg-img-prop-download  mx-auto">
-        <a target="#" class="" href="<?php echo $file['url']; ?>">
-              <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/images/down2.svg" class="" alt="Logo"/>
-                  <p>Calendar</p>
-        </a>
-      </div>
-      <?php endif; ?>
-      <!-- Download Button 1 -->
-      <?php $file = get_field('file-3'); ?>
-      <?php
-      if( $file ): ?>
-      <div class=" col-lg-3 col-11 bg-img-prop-download  mx-auto">
-        <a target="#" class="" href="<?php echo $file['url']; ?>">
-              <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/images/down3.svg" class="" alt="Logo"/>
-                  <p><?php the_field('file-3-name'); ?></p>
-        </a>
-      </div>
-      <?php endif; ?>
-      <!-- Download Button 1 -->
-      <?php $file = get_field('file-4'); ?>
-      <?php
-      if( $file ): ?>
-      <div class=" col-lg-3 col-11 bg-img-prop-download  mx-auto">
-        <a target="#" class="" href="<?php echo $file['url']; ?>">
-              <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/images/down4.svg" class="" alt="Logo"/>
-                  <p><?php the_field('file-4-name'); ?></p>
-        </a>
-      </div>
-      <?php endif; ?>
+      <div class="col-lg-3-52 notice mx-auto  text-center">
+        <h3>Notice</h3>
 
+          <!-- Download Button 1 -->
+          <?php $file = get_field('admission_form'); ?>
+          <?php
+          if( $file ): ?>
+          <div class="download down-notice" onclick="location.href='<?php echo $file['url']; ?>';">
+            <a target="#" class="" href="<?php echo $file['url']; ?>">
+            <p>Admission Form</p>
+            <i class="material-icons"> cloud_download </i>
+          </a>
+          </div>
+          <?php endif; ?>
+          <!-- Download Button 1 -->
+          <?php $file = get_field('admission_form'); ?>
+          <?php
+          if( $file ): ?>
+          <div class="download down-notice" onclick="location.href='<?php echo $file['url']; ?>';">
+            <a target="#" class="" href="<?php echo $file['url']; ?>">
+            <p>Admission Form</p>
+            <i class="material-icons"> cloud_download </i>
+          </a>
+          </div>
+          <?php endif; ?>
+          <!-- Download Button 1 -->
+          <?php $file = get_field('admission_form'); ?>
+          <?php
+          if( $file ): ?>
+          <div class="download down-notice" onclick="location.href='<?php echo $file['url']; ?>';">
+            <a target="#" class="" href="<?php echo $file['url']; ?>">
+            <p>Admission Form</p>
+            <i class="material-icons"> cloud_download </i>
+          </a>
+          </div>
+          <?php endif; ?>
+          <!-- Download Button 1 -->
+          <?php $file = get_field('admission_form'); ?>
+          <?php
+          if( $file ): ?>
+          <div class="download down-notice" onclick="location.href='<?php echo $file['url']; ?>';">
+            <a target="#" class="" href="<?php echo $file['url']; ?>">
+            <p>Admission Form</p>
+            <i class="material-icons"> cloud_download </i>
+          </a>
+          </div>
+          <?php endif; ?>
 
-
-
-</div>
-<div class="row mx-0">
-
-
-      <!-- Download Button 1 -->
-      <?php $file = get_field('file-5'); ?>
-      <?php
-      if( $file ): ?>
-      <div class=" col-lg-3 col-11 bg-img-prop-download  mx-auto">
-        <a target="#" class="" href="<?php echo $file['url']; ?>">
-              <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/images/down5.svg" class="" alt="Logo"/>
-                  <p><?php the_field('file-5-name'); ?></p>
-        </a>
       </div>
-      <?php endif; ?>
-      <!-- Download Button 1 -->
-      <?php $file = get_field('file-6'); ?>
-      <?php
-      if( $file ): ?>
-      <div class=" col-lg-3 col-11 bg-img-prop-download  mx-auto">
-        <a target="#" class="" href="<?php echo $file['url']; ?>">
-              <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/images/down4.svg" class="" alt="Logo"/>
-                  <p><?php the_field('file-6-name'); ?></p>
-        </a>
+
+<!-- ///////////////////////////////////// CLASS ROUTINE //////////////////////////////////////// -->
+
+      <div class="col-lg-3-52 notice mx-auto  text-center">
+        <h3>Class Routine</h3>
+
       </div>
-      <?php endif; ?>
-      <!-- Download Button 1 -->
-      <?php $file = get_field('file-7'); ?>
-      <?php
-      if( $file ): ?>
-      <div class=" col-lg-3 col-11 bg-img-prop-download  mx-auto">
-        <a target="#" class="" href="<?php echo $file['url']; ?>">
-              <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/images/down1.svg" class="" alt="Logo"/>
-                  <p><?php the_field('file-7-name'); ?></p>
+
+<!-- ///////////////////////////////////// Resources //////////////////////////////////////// -->
+      <div class="col-lg-3-52 notice mx-auto  text-center">
+        <h3>Resources</h3>
+
+        <!-- Download Button 1 -->
+        <?php $file = get_field('admission_form'); ?>
+        <?php
+        if( $file ): ?>
+        <div class="download down-resources" onclick="location.href='<?php echo $file['url']; ?>';">
+          <a target="#" class="" href="<?php echo $file['url']; ?>">
+          <p>Admission Form</p>
+          <i class="material-icons"> cloud_download </i>
         </a>
-      </div>
-      <?php endif; ?>
-      <!-- Download Button 1 -->
-      <?php $file = get_field('file-8'); ?>
-      <?php
-      if( $file ): ?>
-      <div class=" col-lg-3 col-11 bg-img-prop-download  mx-auto">
-        <a target="#" class="" href="<?php echo $file['url']; ?>">
-              <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/images/down3.svg" class="" alt="Logo"/>
-                  <p><?php the_field('file-8-name'); ?></p>
+        </div>
+        <?php endif; ?>
+        <!-- Download Button 1 -->
+        <?php $file = get_field('admission_form'); ?>
+        <?php
+        if( $file ): ?>
+        <div class="download down-resources" onclick="location.href='<?php echo $file['url']; ?>';">
+          <a target="#" class="" href="<?php echo $file['url']; ?>">
+          <p>Calendar</p>
+          <i class="material-icons"> cloud_download </i>
         </a>
+        </div>
+        <?php endif; ?>
+        <!-- Download Button 1 -->
+        <?php $file = get_field('admission_form'); ?>
+        <?php
+        if( $file ): ?>
+        <div class="download down-resources" onclick="location.href='<?php echo $file['url']; ?>';">
+          <a target="#" class="" href="<?php echo $file['url']; ?>">
+          <p>Prospectus</p>
+          <i class="material-icons"> cloud_download </i>
+        </a>
+        </div>
+        <?php endif; ?>
+        <!-- Download Button 1 -->
+        <?php $file = get_field('admission_form'); ?>
+        <?php
+        if( $file ): ?>
+        <div class="download down-resources" onclick="location.href='<?php echo $file['url']; ?>';">
+          <a target="#" class="" href="<?php echo $file['url']; ?>">
+          <p>Almanac</p>
+          <i class="material-icons"> cloud_download </i>
+        </a>
+        </div>
+        <?php endif; ?>
+
       </div>
-      <?php endif; ?>
 
 
 
